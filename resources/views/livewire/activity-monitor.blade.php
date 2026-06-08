5f8220a207736217e91f0b4bf55eeb8226ad21df
Black Warrior <dev@blackwarrior.ai>
2026-06-08 17:11:12 +0500

    fix: correct CoolifyTask namespace in activity monitor

--- resources/views/livewire/activity-monitor.blade.php
+++ resources/views/livewire/activity-monitor.blade.php
@@ -1,4 +1,4 @@
-@php use App\Actions\BlackTask\RunRemoteProcess; @endphp
+@php use App\Actions\CoolifyTask\RunRemoteProcess; @endphp
 <div @class([
     'h-full flex flex-col overflow-hidden' => $fullHeight,
     'h-full overflow-hidden' => !$fullHeight,
